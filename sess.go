cbfd6d334b9a0ada8243455002af4b9474e73f3a
peterq <me@peterq.cn>
2024-03-10 18:08:09 +0800

    feat: new conn4

--- sess.go
+++ sess.go
@@ -1166,6 +1166,11 @@ func DialWithOptions(raddr string, block BlockCrypt, dataShards, parityShards in
 	return newUDPSession(convid, dataShards, parityShards, nil, conn, true, udpaddr, block), nil
 }
 
+// NewConn4 establishes a session and talks KCP protocol over a packet connection.
+func NewConn4(convid uint32, raddr net.Addr, block BlockCrypt, dataShards, parityShards int, conn net.PacketConn) (*UDPSession, error) {
+	return newUDPSession(convid, dataShards, parityShards, nil, conn, true, raddr, block), nil
+}
+
 // NewConn3 establishes a session and talks KCP protocol over a packet connection.
 func NewConn3(convid uint32, raddr net.Addr, block BlockCrypt, dataShards, parityShards int, conn net.PacketConn) (*UDPSession, error) {
 	return newUDPSession(convid, dataShards, parityShards, nil, conn, false, raddr, block), nil
